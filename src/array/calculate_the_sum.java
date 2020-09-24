e5860d6c4ee9f3c4fe342af936df97cccfac36e5
Siddharth Vashisht <svashisht008@gmail.com>
2020-09-24 22:30:09 +0530

    Update calculate_the_sum.java

--- src/array/calculate_the_sum.java
+++ src/array/calculate_the_sum.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class calculate_the_sum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner scn=new Scanner(System.in);
 		int n=scn.nextInt();
 		int[] arr=new int[n];
